fb6de885ca44fb9334b9c36a1ea5a4927cd58715
eanderson <717eanderson@gmail.com>
2021-09-03 22:04:19 -0400

    migrated from grid size of 64 to double-precision positioning

--- DisplayWindow.java
+++ DisplayWindow.java
@@ -33,44 +33,12 @@ public class DisplayWindow extends JFrame
 		graphics.setColor(Color.BLACK);
 		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		
-		//show map
-		for (int x = 0; x < game.worldWidth; x++)
-		{
-			for (int y = 0; y < game.worldHeight; y++)
-			{
-				if (game.world[y][x] == 0) {
-					graphics.setColor(Color.GRAY);
-				} else {
-					graphics.setColor(Color.RED);
-				}
-				graphics.fillRect(x * 50, y * 50, 50, 50);
-			}
-		}
-		
-		graphics.setColor(Color.BLUE);
-		for (int x = 0; x < game.worldWidth; x++)
-		{
-			graphics.drawLine(x * 50, 0, x * 50, 450);
-		}
-		for (int y = 0; y < game.worldHeight; y++)
-		{
-			graphics.drawLine(0, y * 50, 800, y * 50);
-		}
-		
-		
-		graphics.setColor(Color.YELLOW);
-		graphics.fillOval((int)(game.xPos / 64d * 50 - 15), (int)(game.yPos / 64d * 50 - 15), 30, 30);
-		
-		//iterate through every vertical strip on the screen
-		
-		
-		graphics.setColor(Color.BLACK);
-		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		
 		graphics.setColor(Color.GREEN);
 		graphics.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight() / 2);
 		graphics.setColor(Color.CYAN);
 		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
+		
+		//iterate through every vertical strip on the screen
 		
 		for (int i = 0; i < 800; i++)
 		{
@@ -87,35 +55,35 @@ public class DisplayWindow extends JFrame
 		boolean facingUp = (angle < 0 || angle > Math.PI);
 		boolean facingLeft = (angle > Math.PI / 2 && angle < 3 * Math.PI / 2);
 
-		int currentWallY = game.yPos / 64 * 64; 
+		double currentWallY = (int)game.yPos;
 		if (facingUp) {
-			currentWallY -= 1;
+			currentWallY -= 0.001;
 		} else {
-			currentWallY += 64;
+			currentWallY += 1;
 		}
 		
-		int currentWallX = (int)(game.xPos + ((double)game.yPos - currentWallY) / Math.tan(-angle));
+		double currentWallX = game.xPos + (game.yPos - currentWallY) / Math.tan(-angle);
 		
-		int deltaY = 64;
+		double deltaY = 1.0;
 		if (facingUp) {
-			deltaY = -64;
+			deltaY = -1.0;
 		}
 		
-		int deltaX = (int)(64d / Math.tan(angle));
+		double deltaX = 1 / Math.tan(angle);
 		if (facingUp) {
-			deltaX = (int)(64d / Math.tan(-angle));
+			deltaX = 1 / Math.tan(-angle);
 		}
 		
 		graphics.setColor(Color.BLUE);
 		//graphics.fillRect(currentWallX / 64 * 50, currentWallY / 64 * 50, 50, 50);
 		
-		int distance = 10000;
+		double distance = 10000;
 		
-		while((currentWallX > 0 && currentWallX < game.worldWidth * 64) && (currentWallY > 0 && currentWallY < game.worldHeight * 64))
+		while((currentWallX > 0 && currentWallX < game.worldWidth) && (currentWallY > 0 && currentWallY < game.worldHeight))
 		{
-			if (game.world[currentWallY/ 64][currentWallX / 64] == 1) {
+			if (game.world[(int)currentWallY][(int)currentWallX] == 1) {
 				//graphics.drawLine((int)(game.xPos / 64d * 50), (int)(game.yPos / 64d * 50), (int)(currentWallX / 64d * 50), (int)(currentWallY / 64d * 50));
-				distance = (int)Math.sqrt(Math.pow(currentWallX - game.xPos, 2) + Math.pow(currentWallY - game.yPos, 2));
+				distance = Math.sqrt(Math.pow(currentWallX - game.xPos, 2) + Math.pow(currentWallY - game.yPos, 2));
 				break;
 			}
 			currentWallX += deltaX;
@@ -124,33 +92,33 @@ public class DisplayWindow extends JFrame
 			//graphics.fillRect(currentWallX / 64 * 50, currentWallY / 64 * 50, 50, 50);
 		}
 		
-		int currentWallX2 = game.xPos / game.gridSize * game.gridSize;
+		double currentWallX2 = (int)game.xPos;
 		if (facingLeft) {
-			currentWallX2 -= 1;
-			deltaX = -game.gridSize;
+			currentWallX2 -= 0.001;
+			deltaX = -1;
 		} else {
-			currentWallX2 += game.gridSize;
-			deltaX = game.gridSize;
+			currentWallX2 += 1;
+			deltaX = 1;
 		}
 		
-		int currentWallY2 = (int)(game.yPos + (game.xPos - currentWallX2) * Math.tan(-angle));
+		double currentWallY2 = game.yPos + (game.xPos - currentWallX2) * Math.tan(-angle);
 		
 		if (facingLeft) {
-			deltaY = (int)(game.gridSize * Math.tan(-angle));
+			deltaY = Math.tan(-angle);
 		} else {
-			deltaY = (int)(game.gridSize * Math.tan(angle));
+			deltaY = Math.tan(angle);
 		}
 		
 		boolean vertHit = false;
 		
-		while((currentWallX2 > 0 && currentWallX2 < game.worldWidth * 64) && (currentWallY2 > 0 && currentWallY2 < game.worldHeight * 64))
+		while((currentWallX2 > 0 && currentWallX2 < game.worldWidth) && (currentWallY2 > 0 && currentWallY2 < game.worldHeight))
 		{
-			if (game.world[currentWallY2 / 64][currentWallX2 / 64] == 1) {
-				int dist = (int)Math.sqrt(Math.pow(currentWallX2 - game.xPos, 2) + Math.pow(currentWallY2 - game.yPos, 2));
+			if (game.world[(int)currentWallY2][(int)currentWallX2] == 1) {
+				double dist = Math.sqrt(Math.pow(currentWallX2 - game.xPos, 2) + Math.pow(currentWallY2 - game.yPos, 2));
 				if (dist < distance) {
 					//graphics.drawLine((int)(game.xPos / 64d * 50), (int)(game.yPos / 64d * 50), (int)(currentWallX2 / 64d * 50), (int)(currentWallY2 / 64d * 50));				
 					vertHit = true;
-					distance = (int)Math.sqrt(Math.pow(currentWallX2 - game.xPos, 2) + Math.pow(currentWallY2 - game.yPos, 2));
+					distance = Math.sqrt(Math.pow(currentWallX2 - game.xPos, 2) + Math.pow(currentWallY2 - game.yPos, 2));
 					break;
 				}
 			}
@@ -160,14 +128,14 @@ public class DisplayWindow extends JFrame
 			//graphics.fillRect(currentWallX2 / 64 * 50, currentWallY2 / 64 * 50, 50, 50);
 		}
 		
-		distance = (int)(distance * Math.cos(angle - game.dir));
+		distance = distance * Math.cos(angle - game.dir);
 		
 		graphics.setColor(Color.DARK_GRAY);
 		if (vertHit) {
 			graphics.setColor(Color.GRAY);
 		}
 		
-		int lineHeight = (int)(64d / distance * 200);
+		int lineHeight = (int)(1 / distance * 200);
 		
 			if (distance < 1000) {
 				graphics.fillRect(i, (int)((canvas.getHeight() - lineHeight) / 2), 1, lineHeight);
@@ -205,9 +173,9 @@ public class DisplayWindow extends JFrame
 		}
 		
 		graphics.setColor(Color.YELLOW);
-		graphics.fillOval((int)(game.xPos / 64d * 10 - 5), (int)(game.yPos / 64d * 10 - 5), 10, 10);
-		graphics.drawLine((int)(game.xPos / 64d * 10), (int)(game.yPos / 64d * 10), (int)(game.xPos / 64d * 10 + Math.cos(game.dir - game.FOV / 2) * 50), (int)(game.yPos / 64d * 10 + Math.sin(game.dir - game.FOV / 2) * 50));
-		graphics.drawLine((int)(game.xPos / 64d * 10), (int)(game.yPos / 64d * 10), (int)(game.xPos / 64d * 10 + Math.cos(game.dir + game.FOV / 2) * 50), (int)(game.yPos / 64d * 10 + Math.sin(game.dir + game.FOV / 2) * 50));
+		graphics.fillOval((int)(game.xPos * 10 - 5), (int)(game.yPos * 10 - 5), 10, 10);
+		graphics.drawLine((int)(game.xPos * 10), (int)(game.yPos * 10), (int)(game.xPos * 10 + Math.cos(game.dir - game.FOV / 2) * 50), (int)(game.yPos * 10 + Math.sin(game.dir - game.FOV / 2) * 50));
+		graphics.drawLine((int)(game.xPos * 10), (int)(game.yPos * 10), (int)(game.xPos * 10 + Math.cos(game.dir + game.FOV / 2) * 50), (int)(game.yPos * 10 + Math.sin(game.dir + game.FOV / 2) * 50));
 		
 		graphics.dispose();
 		bufferStrat.show();
--- Game.java
+++ Game.java
@@ -19,9 +19,8 @@ public class Game
 	
 	public int worldWidth = 16;
 	public int worldHeight = 9;
-	public int gridSize = 64;
-	public int xPos = 96;
-	public int yPos = 224;
+	public double xPos = 10;
+	public double yPos = 3;
 	public double dir = 1.047198;
 	public double FOV = Math.PI / 2;
 	public double maximumSpeed = 0.02;
@@ -53,34 +52,35 @@ public class Game
 			if (input.isPressed(KeyEvent.VK_W)) {
 				
 				if (dir >= Math.PI / 4 && dir < 3 * Math.PI / 4) {
-					yPos += 1;
+					yPos += 0.1;
 				} else if (dir >= 3 * Math.PI / 4 && dir < 5 * Math.PI / 4) {
-					xPos -= 1;
+					xPos -= 0.1;
 				} else if (dir >= 5 * Math.PI / 4 && dir < 7 * Math.PI / 4) {
-					yPos -= 1;
+					yPos -= 0.1;
 				} else {
-					xPos += 1;
+					xPos += 0.1;
 				}
 				
 				timeSinceLastMove = maximumSpeed;
 			} else if (input.isPressed(KeyEvent.VK_S)) {
 				if (dir >= Math.PI / 4 && dir < 3 * Math.PI / 4) {
-					yPos -= 1;
+					yPos -= 0.1;
 				} else if (dir >= 3 * Math.PI / 4 && dir < 5 * Math.PI / 4) {
-					xPos += 1;
+					xPos += 0.1;
 				} else if (dir >= 5 * Math.PI / 4 && dir < 7 * Math.PI / 4) {
-					yPos += 1;
+					yPos += 0.1;
 				} else {
-					xPos -= 1;
+					xPos -= 0.1;
 				}
 				
 				timeSinceLastMove = maximumSpeed;
 			}
-			xPos = Math.min(xPos, worldWidth * gridSize - gridSize / 2);
-			xPos = Math.max(xPos, gridSize / 2);
+			/*
+			xPos = Math.min(xPos, worldWidth - 0.5);
+			xPos = Math.max(xPos, 0.5);
 			
-			yPos = Math.min(yPos, worldHeight * gridSize - gridSize / 2);
-			yPos = Math.max(yPos, gridSize / 2);
+			yPos = Math.min(yPos, worldHeight - 0.5);
+			yPos = Math.max(yPos, 0.5);*/
 		}
 		
 		
